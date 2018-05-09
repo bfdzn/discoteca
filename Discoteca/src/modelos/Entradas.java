633dcda6d4557996424ba182afb138809be88a3e
admin <admin@Win7_1TB>
2018-05-09 13:44:34 +0300

    .

--- Discoteca/src/modelos/Entradas.java
+++ Discoteca/src/modelos/Entradas.java
@@ -1,5 +1,26 @@
 package modelos;
 
+import java.time.LocalDate;
+
 public class Entradas {
+	
+	private int numEntrada;
+	private String dniEntrada;
+	private int idEspectaculo;
+	private LocalDate fecha;
+	private String vendedor;
+	
+	
+	public Entradas(int numEntrada, String dniEntrada, int idEspectaculo, LocalDate fecha, String vendedor) {
+		super();
+		this.numEntrada = numEntrada;
+		this.dniEntrada = dniEntrada;
+		this.idEspectaculo = idEspectaculo;
+		this.fecha = fecha;
+		this.vendedor = vendedor;
+	}
+
+
+	
 
 }
--- Discoteca/src/modelos/Espectaculos.java
+++ Discoteca/src/modelos/Espectaculos.java
@@ -1,5 +1,17 @@
 package modelos;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class Espectaculos{
 
+	
+		private int idEspectaculo;
+		private String nombre;
+		private LocalDate fechaInicio;
+		private LocalDate fechaFin;
+		
+		
+		
 }
