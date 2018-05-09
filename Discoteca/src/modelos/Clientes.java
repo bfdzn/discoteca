e27ece2a68c21e54d76b349f6b82303ab4b5c949
Enrique Zarco <Enrique Zarco@DESKTOP-0DI6CSK>
2018-05-09 12:50:52 +0200

    Clase Empleados

---
+++ Discoteca/src/modelos/Clientes.java
@@ -0,0 +1,31 @@
+package modelos;
+
+import java.time.LocalDate;
+
+public class Clientes {
+	private String dni;
+	private LocalDate fechaNacimiento;
+	
+	public Clientes(String dni, LocalDate fechaNacimiento) {
+		super();
+		this.dni = dni;
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+}
