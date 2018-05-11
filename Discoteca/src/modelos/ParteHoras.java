218b550138b56f382b71f13a6d11ff07e14f3cd7
admin <admin@Win7_1TB>
2018-05-11 09:39:55 +0300

    parte horas

--- Discoteca/src/modelos/ParteHoras.java
+++ Discoteca/src/modelos/ParteHoras.java
@@ -1,10 +1,63 @@
+/*Tomás Gonzálvez*/
+
 package modelos;
 
 import java.time.LocalDate;
 
 public class ParteHoras {
-	String dniEmpleado;
-	LocalDate fecha;
+	private String dniEmpleado;
+	private LocalDate fecha;
+	private Double horas;
+	private double salario;
+	
+	
+	public ParteHoras(String dniEmpleado, LocalDate fecha, Double horas, double salario) {
+		super();
+		this.dniEmpleado = dniEmpleado;
+		this.fecha = fecha;
+		this.horas = horas;
+		this.salario = salario;
+	}
+
+
+	public String getDniEmpleado() {
+		return dniEmpleado;
+	}
+
+
+	public void setDniEmpleado(String dniEmpleado) {
+		this.dniEmpleado = dniEmpleado;
+	}
+
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+
+	public Double getHoras() {
+		return horas;
+	}
+
+
+	public void setHoras(Double horas) {
+		this.horas = horas;
+	}
+
+
+	public double getSalario() {
+		return salario;
+	}
+
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
 	
 	
 }
