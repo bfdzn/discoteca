31e06caf97aa8a123d1700b7e4d3b606908bc4d8
borja <borja@mbp-de-borja>
2018-05-11 10:40:36 +0200

    hhhh

--- Discoteca/src/modelos/Admin.java
+++ Discoteca/src/modelos/Admin.java
@@ -6,7 +6,8 @@ public class Admin extends Empleado {
 
 	public Admin(String dni, String nombre, String apellido, String oficio, LocalDate fechaAlta, String contraseña) {
 		super(dni, nombre, apellido, oficio, fechaAlta, contraseña);
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub 
+		//frvgs
 	}
 
 	
