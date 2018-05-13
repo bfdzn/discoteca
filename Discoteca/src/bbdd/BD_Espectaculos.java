5af9f17ed64e40e8d6372cbdd88cf5e105a11ee0
HP <HP@HP-PC>
2018-05-13 18:08:29 +0200

    .

--- Discoteca/src/bbdd/BD_Espectaculos.java
+++ Discoteca/src/bbdd/BD_Espectaculos.java
@@ -20,9 +20,10 @@ public class BD_Espectaculos extends BD_Conector {
 
 
 
-	public  int añadir_Cliente( Espectaculos espectaculo){	
-		String cadenaSQL="INSERT INTO CLIENTES2 VALUES('" + espectaculo.getIdEspectaculo() + "','" +
-				espectaculo.getNombreEspectaculo() + "','" + espectaculo.getFechaInicio() + "','" + espectaculo.getFechaFin() + "')"; 	
+	public  int añadir_Espectaculo( Espectaculos espectaculo){	
+		String cadenaSQL="INSERT INTO ESPECTACULOS VALUES('" + espectaculo.getIdEspectaculo() + "','" +
+				espectaculo.getNombreEspectaculo() + "','" + espectaculo.getFechaInicio() + "','" + espectaculo.getFechaFin() + "')"; 
+		
 		try{
 			this.abrir();
 			s=c.createStatement();
@@ -36,5 +37,46 @@ public class BD_Espectaculos extends BD_Conector {
 			}
 	}
 	
+	
+	public int borrar_Espectaculo (Espectaculos espectaculo) {
+		
+		String cadenaSQL="DELETE FROM ESPECTACULOS WHERE  idespectaculo= ' " + espectaculo.getIdEspectaculo() + "'";
+		
+		try{
+			this.abrir();
+			s=c.createStatement();
+			int filas=s.executeUpdate(cadenaSQL);
+			s.close();
+			this.cerrar();
+			return filas;
+			}
+			catch ( SQLException e){			
+				return -1;
+			
+		
+			}
+	}
+	
+	public String buscarEspectaculo (Espectaculos espectaculo) {
+		
+		String cadena="SELECT * FROM ESPECTACULOS2 WHERE idespectaculo = ' " +espectaculo.getIdEspectaculo() + " ' ";
+		
+		try{
+			String t="";
+			this.abrir();
+			s=c.createStatement();
+			reg=s.executeQuery(cadena);
+			if ( reg.next())							
+				t= reg.getString(1);							
+			s.close();
+			this.cerrar();
+			return t;
+		}
+		catch ( SQLException e){
+	
+			return null;
+			
+		}
+	}
 	
 }
